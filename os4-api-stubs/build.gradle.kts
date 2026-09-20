6f149e021e44c2e7469047cf9fa2b3ef64633702
Maga-King <2806379025@qq.com>
2026-09-20 14:26:20 +0800

    Allow clean checkout builds with Android SDK environment variables

--- os4-api-stubs/build.gradle.kts
+++ os4-api-stubs/build.gradle.kts
@@ -2,9 +2,14 @@ import java.util.Properties
 
 plugins { `java-library` }
 val sdkProperties = Properties().apply {
-    rootProject.file("local.properties").inputStream().use { load(it) }
+    val local = rootProject.file("local.properties")
+    if (local.isFile) local.inputStream().use { load(it) }
 }
-dependencies { compileOnly(files("${sdkProperties.getProperty("sdk.dir")}/platforms/android-36/android.jar")) }
+val sdkDirectory = sdkProperties.getProperty("sdk.dir")
+    ?: providers.environmentVariable("ANDROID_HOME").orNull
+    ?: providers.environmentVariable("ANDROID_SDK_ROOT").orNull
+    ?: error("Set ANDROID_HOME or sdk.dir in local.properties to your Android SDK directory")
+dependencies { compileOnly(files("$sdkDirectory/platforms/android-36/android.jar")) }
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
